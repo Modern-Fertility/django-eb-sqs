a33025622a611360e986897c99745fa8d7aaa2f1
Will McCutchen <will.mccutchen@ro.co>
2025-02-25 16:41:32 -0500

    fix: django 4.x incompatibility

--- eb_sqs/urls.py
+++ eb_sqs/urls.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.conf.urls import url
+from django.urls import re_path
 
 from eb_sqs.views import process_task
 
 urlpatterns = [
-    url(r'^process$', process_task, name='process_task'),
+    re_path(r"^process$", process_task, name="process_task"),
 ]
